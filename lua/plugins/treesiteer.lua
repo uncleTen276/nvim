66bea2ab862c10676ebe97ab9eed7969ad398cee
sekke276 <tringuyen2762001@gmail.com>
2024-04-24 17:35:15 +0700

    fix cmp

--- lua/plugins/treesiteer.lua
+++ lua/plugins/treesiteer.lua
@@ -13,40 +13,37 @@ return {
     { "<leader>Tu", "<cmd>TSUpdateSync", desc = "Treesitter Update sync" },
   },
   event = { "BufRead", "VeryLazy" },
-  opts = function()
-    return {
-      indent = true,
-      ensure_installed = {
-        "bash",
-        "comment",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "regex",
-        "rust",
-        "scss",
-        "svelte",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "c_sharp",
-      },
-      autotag = {
-        enable = true,
-      },
-      rainbow = {
-        enable = true,
-      },
-      playground = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    }
-  end,
+  opts = {
+    indent = true,
+    ensure_installed = {
+      "bash",
+      "comment",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "rust",
+      "scss",
+      "svelte",
+      "toml",
+      "tsx",
+      "typescript",
+      "yaml",
+      "c_sharp",
+    },
+    autotag = {
+      enable = true,
+    },
+    rainbow = {
+      enable = true,
+    },
+    playground = {
+      enable = true,
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+  },
 }
